b5c4677e0c0eea7c3d2dcdb470a72f3f9467ad75
TrevorThebe <trevztm@gmail.com>
2025-07-19 10:35:41 +0200

    Update Bakery.tsx

--- src/pages/Bakery.tsx
+++ src/pages/Bakery.tsx
@@ -4,8 +4,8 @@ import { ProductListByLocation } from '@/components/ProductListByLocation';
 const BakeryPage: React.FC = () => (
   <div className="p-6">
     <h1 className="text-2xl font-bold mb-6">Bakery Products</h1>
-<ProductListByLocation locationName="Bakery" />
-    </div> 
+    <ProductListByLocation locationName="Bakery" />
+  </div>
 );
 
 export default BakeryPage;
