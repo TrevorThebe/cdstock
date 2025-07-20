708a45ac721eebc6ad144c7a938981c8595e5bc8
TrevorThebe <trevztm@gmail.com>
2025-07-20 19:48:07 +0200

    hide debug
    hide

--- src/components/ProductListByLocation.tsx
+++ src/components/ProductListByLocation.tsx
@@ -183,8 +183,8 @@ export const ProductListByLocation: React.FC<{ locationName: string }> = ({ loca
           </div>
           {debugInfo && (
             <div className="mt-6 p-4 bg-gray-100 rounded-md text-sm d-none">
-              <h3 className="font-medium mb-2">Debug Info:</h3>
-              <pre className="text-xs">{JSON.stringify(debugInfo, null, 2)}</pre>
+              <h3 className="font-medium mb-2 d-none">Debug Info:</h3>
+              <pre className="text-xs d-none">{JSON.stringify(debugInfo, null, 2)}</pre>
             </div>
           )}
         </>
